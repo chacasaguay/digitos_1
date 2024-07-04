f3ea62dd6662d7ba277961ff147208b5cad3cb98
chacasaguay <158518298+chacasaguay@users.noreply.github.com>
2024-07-04 16:45:01 -0500

    digitos_1.R

--- R/digito.R
+++ R/digito.R
@@ -1,15 +1,15 @@
-digitos<-function(Numero_3_digitos){
-  N<-Numero_3_digitos
-  if( N>-100 & N<100){
-    dig1<-trunc(N/100)
-    x1<- N %% 100
-    dig2<-trunc(x1/10)
-    dig3<-x1%% 10
+digitos<- function(Numero_3_digitos){
+  N<- Numero_3_digitos
+  if(N>=100 & N < 1000){
+    dig1<- trunc(N/100)
+    x1 <-N%% 100
+    dig2 <- trunc(x1/10)
+    dig3 <- x1%%10
     s <- dig1+dig2+dig3
-    print("La suma de los digitoses:")
+    print("LA SUMA DE LOS DIGITOS ES :")
     print(s)
+    }else{
+      print("no es un numero de 3 digitos ")
+    }
+
   }
-  else  {
-    print("No es numeo de 3 digitos")
-}
-}
